f39df409027062ec58769782d6de0239ea84de3e
Andy Huang <andy.huang7@student.tdsb.on.ca>
2024-10-24 13:33:57 -0400

    made the Coslaw calculator

---
+++ script.js
@@ -0,0 +1,107 @@
+
+
+function Coslaw() {
+    const a = parseFloat(document.getElementById("side_a").value);
+    const b = parseFloat(document.getElementById("side_b").value);
+    const C = parseFloat(document.getElementById("Angle").value) * (Math.PI / 180);
+    
+    const c_length = Math.sqrt(a * a + b * b - (2 * a * b * Math.cos(C)));
+    document.getElementById("c_length").value = c_length.toFixed(2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let sum = 0;
+for (let i = 0; i <= navigator; i++) {
+  sum += formula
+}
